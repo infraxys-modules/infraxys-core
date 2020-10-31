ca1fc953314516420b96b34a19f82839f1f0778c
Jeroen Manders <jeroen@manders.be>
2020-10-31 14:43:05 +0000

    Fix invalid usage of stderr

--- python/infraxys/os_utils.py
+++ python/infraxys/os_utils.py
@@ -12,6 +12,7 @@ class OsUtils(object):
     def run_command_over_ssh(hostname, command_line, workdir=None, hide_stdout=False, hide_stderr=False, exit_on_error=False, log_command=True):
         return OsUtils.run_command(f'ssh -k {hostname}', command_line, workdir, hide_stdout, hide_stderr, exit_on_error)
 
+    @staticmethod
     def run_command(command_line, arguments=None, workdir=None, hide_stdout=False, hide_stderr=False,
                     exit_on_error=False, log_command=True, string_for_stdin=None):
         if log_command:
@@ -39,17 +40,21 @@ class OsUtils(object):
             if hide_stderr:
                 stderr = dev_null
 
-        process = subprocess.Popen(command_parts, stdout=stdout, stderr=stderr, stdin=stdin)
+        process = subprocess.Popen(
+            command_parts, stdout=stdout, stderr=stderr, stdin=stdin)
         if string_for_stdin:
-            stdout_result = process.communicate(input=str.encode(string_for_stdin))[0].decode()
+            stdout_result = process.communicate(
+                input=str.encode(string_for_stdin))[0].decode()
         else:
             stdout_result = process.communicate()[0].decode()
 
-        stderr_result = process.communicate()[1].decode()
         exitcode = process.returncode
+        perror = process.communicate()[1]
+        stderr_result = perror.decode() if perror else ''
 
         if exitcode != 0 and exit_on_error:
-            OsUtils.logger.error(f'Exit code {exitcode} {stdout_result} {stderr_result}')
+            OsUtils.logger.error(
+                f'Exit code {exitcode} {stdout_result} {stderr_result}')
             sys.exit(1)
 
         return exitcode, stdout_result, stderr_result
@@ -65,4 +70,5 @@ class OsUtils(object):
                     sys.exit(1)
 
         if extra_dir:
-            OsUtils.ensure_dir(f'{path}/{extra_dir}', exit_on_error=exit_on_error)
+            OsUtils.ensure_dir(f'{path}/{extra_dir}',
+                               exit_on_error=exit_on_error)
